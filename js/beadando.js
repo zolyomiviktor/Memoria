68c659fbc80c5ad0e03d4eadf808589aef4785d3
zovik <zovikm@gmail.com>
2022-01-01 15:44:53 +0100

    2022.01.01

---
+++ js/beadando.js
@@ -0,0 +1,150 @@
+var valueS = 0;
+parseInt(valueS)
+
+function addS() {
+    if(valueS>=9){
+      valueS = 0;
+    } else {
+    valueS++;
+    }
+    document.getElementById("S").innerHTML = valueS;
+}
+
+var valueE = 0;
+parseInt(valueE)
+
+function addE() {
+    if(valueE>=9){
+      valueE = 0;
+    } else {
+    valueE++;
+    }
+    document.getElementById("E").innerHTML = valueE;
+}
+var valueE2 = 0;
+parseInt(valueE2)
+
+function addE2() {
+    if(valueE2>=9){
+      valueE2 = 0;
+    } else {
+    valueE2++;
+    document.getElementById("E2").innerHTML = valueE2;
+}
+}
+var valuee = 0;
+parseInt(valuee)
+
+function adde() {
+    if(valuee>=9){
+      valuee = 0;
+    } else {
+    valuee++;
+    }
+    document.getElementById("e").innerHTML = valuee;
+  }
+
+var valueN = 0;
+parseInt(valueN)
+
+function addN() {
+    if(valueN>=9){
+      valueN = 0;
+    } else {
+    valueN++;
+    }
+    document.getElementById("N").innerHTML = valueN;
+}
+var valuen = 0;
+parseInt(valuen)
+
+function addn() {
+    if(valuen>=9){
+      valuen = 0;
+    } else {
+    valuen++;
+    }
+    document.getElementById("n").innerHTML = valuen;
+}
+
+var valueD = 0;
+parseInt(valueD)
+
+function addD() {
+    if(valueD>=9){
+      valueD = 0;
+    } else {
+    valueD++;
+    }
+    document.getElementById("D").innerHTML = valueD;
+}
+
+var valueM = 0;
+parseInt(valueM)
+
+function addM() {
+    if(valueM>=9){
+      valueM = 0;
+    } else {
+    valueM++;
+    }
+    document.getElementById("M").innerHTML = valueM;
+}
+var valuem = 0;
+parseInt(valuem)
+
+function addm() {
+    if(valuem>=9){
+      valuem = 0;
+    } else {
+    valuem++;
+    }
+    document.getElementById("m").innerHTML = valuem;
+}
+
+var valueO = 0;
+parseInt(valueO)
+
+function addO() {
+    if(valueO>=9){
+      valueO = 0;
+    } else {
+    valueO++;
+    }
+    document.getElementById("O").innerHTML = valueO;
+}
+var valueo = 0;
+parseInt(valueo)
+
+function addo() {
+    if(valueo>=9){
+      valueo = 0;
+    } else {
+    valueo++;
+    }
+    document.getElementById("o").innerHTML = valueo;
+}
+ 
+var valueR = 0;
+parseInt(valueR)
+
+function addR() {
+    if(valueR>=9){
+      valueR = 0;
+    } else {
+    valueR++;
+    }
+    document.getElementById("R").innerHTML = valueR;
+}
+
+var valuey = 0;
+parseInt(valuey)
+
+function addy() {
+    if(valuey>=9){
+      valuey = 0;
+    } else {
+    valuey++;
+    }
+    document.getElementById("y").innerHTML = valuey;
+}
